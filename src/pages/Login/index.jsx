b831b66abfd34d111aa4a8fccec913bef7eab5c3
Rakeshv1108 <rakeshv1108@gmail.com>
2025-06-21 10:59:02 +0530

    login page design completed

--- src/pages/Login/index.jsx
+++ src/pages/Login/index.jsx
@@ -4,9 +4,14 @@ import { MyContext } from "../../App";
 import patern from "../../assets/images/pattern.webp";
 import { MdEmail } from "react-icons/md";
 import { RiLockPasswordFill } from "react-icons/ri";
+import { IoMdEye, IoMdEyeOff } from "react-icons/io";
+import Button from "@mui/material/Button";
+import { Link } from "react-router-dom";
+import googleIcon from "../../assets/images/google-icon.png";
 
 const Login = () => {
-  const [inputIndex, setInputIndex] = useState(0);
+  const [inputIndex, setInputIndex] = useState(null);
+  const [isShowPassword, setIsShowPassword] = useState(false);
   const context = useContext(MyContext);
 
   useEffect(() => {
@@ -15,7 +20,7 @@ const Login = () => {
 
   const focusInput = (index) => {
     setInputIndex(index);
-  }
+  };
 
   return (
     <>
@@ -24,12 +29,16 @@ const Login = () => {
         <div className="loginBox">
           <div className="logo text-center">
             <img src={Logo} width="90px" height="90px" />
-            <h5>Login to UdaanPrep</h5>
+            <h4 className="mt-2 mb-4 fw-bold">Login to UdaanPrep</h4>
           </div>
 
-          <div className="wrapper mt-3 card border p-4">
+          <div className="wrapper mt-3 card border">
             <form>
-              <div className={`form-group mb-3 position-relative ${inputIndex === 0 && 'focus'}`}>
+              <div
+                className={`form-group position-relative ${
+                  inputIndex === 0 && "focus"
+                }`}
+              >
                 <span className="icon">
                   <MdEmail />
                 </span>
@@ -42,19 +51,62 @@ const Login = () => {
                 />
               </div>
 
-              <div className={`form-group mb-3 position-relative ${inputIndex === 0 && 'focus'}`}>
+              <div
+                className={`form-group position-relative ${
+                  inputIndex === 1 && "focus"
+                }`}
+              >
                 <span className="icon">
-                  <MdEmail />
+                  <RiLockPasswordFill />
                 </span>
                 <input
-                  type="text"
+                  type={`${isShowPassword ? "text" : "password"}`}
                   className="form-control"
-                  placeholder="enter your email"
-                  onFocus={() => focusInput(0)}
+                  placeholder="enter your password"
+                  onFocus={() => focusInput(1)}
                   onBlur={() => focusInput(null)}
                 />
+                <span
+                  className="toggleShowPassword"
+                  onClick={() => setIsShowPassword(!isShowPassword)}
+                >
+                  {isShowPassword ? <IoMdEyeOff /> : <IoMdEye />}
+                </span>
               </div>
+
+              <div className="form-group">
+                <Button className="btn-blue btn-lg w-100 btn-big">
+                  <span className="text-capitalize">{'Sign In'}</span>
+                </Button>
+              </div>
+
+              <div className="form-group text-center mb-0">
+                <Link to={"/forgot-password"} className="link">
+                  Forgot Password
+                </Link>
+                <div className="d-flex align-items-center justify-content-center or mt-3 m-3">
+                  <span className="line"></span>
+                  <span className="txt">or</span>
+                  <span className="line"></span>
+                </div>
+              </div>
+
+              <Button
+                variant="outlined"
+                className="w-100 btn-lg btn-big loginWithGoogle"
+              >
+                <img src={googleIcon} width="25px" /> &nbsp; Sign In with Google
+              </Button>
             </form>
+          </div>
+
+          <div className="wrapper mt-3 card border footer p-3">
+            <span className="text-center">
+              Don't have an account?
+              <Link to={"/signUp"} className="link color ms-2">
+                Register
+              </Link>
+            </span>
           </div>
         </div>
       </section>
